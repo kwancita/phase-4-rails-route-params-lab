4c5534aa7855c88c7efad577b4193a720d77e448
Wasanta <kwan_u_day@hotmail.com>
2022-01-08 13:50:47 -0500

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,20 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  # def index
+  #   students = if params[:name]
+  #                Student.by_name(params[:name])
+  #              else
+  #                Student.all
+  #              end    
+  #   render json: students
+  # end
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
+
+  
+
 end
